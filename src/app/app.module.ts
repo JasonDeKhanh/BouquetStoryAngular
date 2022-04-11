883ddbb415eb9951ac91fe4dcc2f3b81dedeec07
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2022-04-11 13:23:17 +0800

    Initialised and redirected default page to Index view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
