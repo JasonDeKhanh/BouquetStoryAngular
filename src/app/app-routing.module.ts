9ad18bdd6617e60370a7fbafeae9bb7aeef58c66
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2022-04-11 18:21:29 +0800

    added routings and some imports. added a test button to index. added header to app.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { IndexComponent } from './index/index.component';
+import { FlowerGalleryComponent } from './flower-gallery/flower-gallery.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'index', component: IndexComponent },
+  { path: 'flowerGallery', component: FlowerGalleryComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './header/header.component';
+
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+// You have to import MenuItem in the component where you are going to use it, 
+// you can't import it in app.module.ts because you can't include it in ngmodule imports
+import { TabViewModule } from 'primeng/tabview';
+import { FlowerGalleryComponent } from './flower-gallery/flower-gallery.component';
+import { DataViewModule } from 'primeng/dataview';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    HeaderComponent,
+    FlowerGalleryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ButtonModule,
+    MenubarModule,
+    TabViewModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
