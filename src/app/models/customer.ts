dd711caa8ad1b3f75ddd65e79bdf32229eadeaa8
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2022-04-12 15:21:18 +0800

    Added attributes and constructors for the rest of the entities

--- src/app/models/customer.ts
+++ src/app/models/customer.ts
@@ -1,2 +1,19 @@
+import { SaleTransaction } from "./sale-transaction";
+
 export class Customer {
+
+    customerId: number | undefined;
+    firstName: string | undefined;
+    lastName: string | undefined;
+    username: string | undefined;
+
+    saleTransactions: SaleTransaction[] | undefined;
+
+    constructor(customerId?: number, firstName?: string, lastName?: string, username?: string) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+    }
+
 }
--- src/app/models/sale-transaction-line-item.ts
+++ src/app/models/sale-transaction-line-item.ts
@@ -1,2 +1,19 @@
+import { Item } from "./item";
+
 export class SaleTransactionLineItem {
+
+    saleTransactionLineItemId: number | undefined;
+    serialNumber: number | undefined;
+    quantity: number | undefined;
+    unitPrice: number | undefined;
+
+    item: Item | undefined;
+
+    constructor(saleTransactionLineItemId?: number, serialNumber?: number, quantity?: number, unitPrice?: number) {
+        this.saleTransactionLineItemId = saleTransactionLineItemId;
+        this.serialNumber = serialNumber;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
 }
