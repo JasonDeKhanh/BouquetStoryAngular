e80435fc8b0e55f4b9b2276bd43713fd649d8abd
Qingyi <xqy1115@gmail.com>
2022-04-17 06:56:11 +0800

    checkout v1.0

--- src/app/models/sales-transaction-req.ts
+++ src/app/models/sales-transaction-req.ts
@@ -1,16 +1,23 @@
 import { SaleTransaction } from "./sale-transaction";
 import { SaleTransactionLineItem } from "./sale-transaction-line-item";
+import { Item } from "./item";
 
 export class SalesTransactionReq {
     
     username: String | undefined;
+    firstName: String | undefined;
+    lastName: String | undefined;
     saleTransaction: SaleTransaction | undefined;
     saleTransactionLineItems: SaleTransactionLineItem[]| undefined;
+    items: number[]|undefined;
     
-    constructor(username?: string, saleTransaction?: SaleTransaction, saleTransactionLineItems?:SaleTransactionLineItem[])
+    constructor(username?: string, firstName?: string, lastName?:string, saleTransaction?: SaleTransaction, saleTransactionLineItems?:SaleTransactionLineItem[],  items?: number[])
     {	
         this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.saleTransaction = saleTransaction;
         this.saleTransactionLineItems = saleTransactionLineItems;
+        this.items = items;
     }
 }
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -11,13 +11,18 @@ import { Address } from '../models/address';
 import { CreditCard } from '../models/credit-card';
 import { SaleTransactionService } from '../services/sale-transaction.service';
 import { Customer } from '../models/customer';
-import { DatePipe } from '@angular/common';
+import { DatePipe } from '@angular/common';;
+import { AddressService } from '../services/address.service';
+import { CreditCardService } from '../services/credit-card.service';
+import { Item } from '../models/item';
+
+import {DropdownModule} from 'primeng/dropdown';
 
 @Component({
     selector: 'app-shopping-cart',
     templateUrl: './shopping-cart.component.html',
     styleUrls: ['./shopping-cart.component.css'],
-    providers: [MessageService, ConfirmationService,DatePipe]
+    providers: [MessageService, ConfirmationService,DatePipe, DropdownModule]
 })
 export class ShoppingCartComponent implements OnInit {
 
@@ -33,6 +38,15 @@ export class ShoppingCartComponent implements OnInit {
     creditCard : CreditCard = new CreditCard();
     expiryString : String = "";
     minDateValue : Date = new Date();
+    newCustomerEmail : string;
+
+    newUsername : string;
+    newFirstName : string;
+    newLastName : string;
+
+    addresses : Address[] | undefined;
+    creditCards : CreditCard[] | undefined;
+    items: number[] | undefined;
 
     displayRegisteredGuestCheckoutDialog : boolean = false;
     displayUnregisteredGuestCheckoutDialog : boolean = false;
@@ -40,7 +54,9 @@ export class ShoppingCartComponent implements OnInit {
     constructor(public sessionService: SessionService,
                 private messageService: MessageService, 
                 private confirmationService: ConfirmationService,
-                private saleTransactionService : SaleTransactionService) {
+                private saleTransactionService : SaleTransactionService,
+                private creditCardService : CreditCardService,
+                private addressService : AddressService) {
 
         this.totalLineItem = 0;
         this.totalQuantity = 0;
@@ -50,11 +66,27 @@ export class ShoppingCartComponent implements OnInit {
         this.deliveryAddress = "";
         this.isPreorder = false;
         this.minDateValue.setDate(new Date().getDate() + 3);
-        
-
-        // this.creditCard./
-
+   
         this.saleTransactionLineItems = new Array();
+
+        this.addressService.getAddresses(this.sessionService.getUsername()).subscribe({
+            next:(response)=>{
+                this.addresses = response;
+            },
+            error:(error)=>{
+                console.log('********** ViewAllAddress.ts: ' + error);
+            }
+        });
+
+        this.creditCardService.getcreditCards(this.sessionService.getUsername()).subscribe({
+            next:(response)=>{
+                this.creditCards = response;
+            },
+            error:(error)=>{
+                console.log('********** ViewAllAddress.ts: ' + error);
+            }
+        });
+        
 
     }
 
@@ -62,6 +94,16 @@ export class ShoppingCartComponent implements OnInit {
         this.saleTransactionLineItems = this.sessionService.getCartLineItems();
 
         this.totalLineItem = this.saleTransactionLineItems.length;
+
+        
+        this.items = new Array<number>(this.saleTransactionLineItems.length);
+        
+        for(let i = 0; i<this.saleTransactionLineItems.length;i++){
+
+            this.items[i] = this.saleTransactionLineItems[i].item.itemId;
+            // console.log("---->"+this.items[i].itemId)
+        }
+
         for (var lineItem of this.saleTransactionLineItems) {
             this.totalQuantity += lineItem.quantity ? lineItem.quantity : 0;
             this.totalPriceAmount += (lineItem.quantity && lineItem.unitPrice) ? lineItem.unitPrice * lineItem.quantity : 0;
@@ -77,8 +119,6 @@ export class ShoppingCartComponent implements OnInit {
         this.displayRegisteredGuestCheckoutDialog = false;
         this.displayUnregisteredGuestCheckoutDialog = false;
     }
-
-
 
     getSubtotal(lineItem: SaleTransactionLineItem) {
         return lineItem!.unitPrice! * lineItem!.quantity!;
@@ -130,6 +170,16 @@ export class ShoppingCartComponent implements OnInit {
 
         // registered checkout and unregistered checkout??
 
+    } 
+
+    doUnregisteredCheckout(newSaleTransactionItems : SaleTransactionLineItem[] ){
+        this.displayUnregisteredGuestCheckoutDialog = true;
+
+        this.saleTransactionLineItems = newSaleTransactionItems;
+
+        for (var lineItem of newSaleTransactionItems) {
+            console.log("==>"+lineItem.item.itemId);
+        }
     }
 
     registeredCheckout() {
@@ -137,48 +187,58 @@ export class ShoppingCartComponent implements OnInit {
 
         let transactionDate = new Date().toISOString();
         let collectionDate = this.collectionDateTime.toISOString();
+        let username = this.sessionService.getUsername();
+        let firstName = this.sessionService.getCurrentCustomer().firstName;
+        let lastName = this.sessionService.getCurrentCustomer().lastName;
 
+        var newSaleTransaction: SaleTransaction;
 
+        let validaCheckOut = false;
 
-        // if(this.isSelfPickup==true) {
-                console.log("ssssssssssssssssssssssssssssssssssssss")
-
-                var newSaleTransaction: SaleTransaction;
-                newSaleTransaction = new SaleTransaction(1, this.totalLineItem, this.totalQuantity,
-                    this.totalPriceAmount, transactionDate, collectionDate, this.isSelfPickup,
-                    null, false, this.isPreorder, false);
-
-                this.saleTransactionService.createNewSessionService(this.sessionService.getUsername(), newSaleTransaction, this.saleTransactionLineItems).subscribe({
-                    next: (response) => {
-                        this.messageService.add({severity:'success', summary: 'Successful', detail: 'Checkout!', life: 3000});
-                        this.address = new Address();
-                
-                        this.saleTransactionLineItems = [];
-                
-                        this.displayRegisteredGuestCheckoutDialog = false;
-                    },
-                    error: (error) => {
-                        //   this.message = "An error has occurred while registering new account: \n" + error;
-                        // this.message = "This email is already registered as a customer!";
-                
-                        console.log('********** Checkout.ts: ' + error);
-                    }
-                })
-                
-                
-        // } else if (this.totalLineItem!=0 &&  this.totalQuantity!=0 && this.totalPriceAmount!=0 &&
-        //     this.isSelfPickup==false && this.address.line!="" && this.address.postCode!="") {
-               
-        //         this.deliveryAddress = this.address.line + "; Postal Code: " + this.address.postCode;
-               
-        //         var newSaleTransaction: SaleTransaction;
-        //         newSaleTransaction = new SaleTransaction(1, this.totalLineItem, this.totalQuantity,
-        //             this.totalPriceAmount, new Date(), this.collectionDateTime, this.isSelfPickup,
-        //             this.deliveryAddress, false, this.isPreorder, false);
-        // }
-        
+        if(this.isSelfPickup==true) {
+            console.log("=== register self pickup checkout === ")
+            validaCheckOut = true;
             
+            newSaleTransaction = new SaleTransaction(1, this.totalLineItem, this.totalQuantity,
+                this.totalPriceAmount, transactionDate, collectionDate, this.isSelfPickup,
+                null, false, this.isPreorder, false);
+            
+        } else if(this.isSelfPickup==false && this.address.line!='' && this.address.postCode!=''){
+            console.log("=== register delivery checkout === ")
+            validaCheckOut = true;
+            this.deliveryAddress = this.address.line + "; Postal Code: "+this.address.postCode;
+            
+            newSaleTransaction = new SaleTransaction(1, this.totalLineItem, this.totalQuantity,
+                this.totalPriceAmount, transactionDate, collectionDate, this.isSelfPickup,
+                this.deliveryAddress, false, this.isPreorder, false);
+        }
 
+        if(validaCheckOut) {
+            this.saleTransactionService.createNewSessionService(username, firstName, lastName, newSaleTransaction, 
+                this.saleTransactionLineItems, this.items).subscribe({
+                next: (response) => {
+                    this.messageService.add({severity:'success', summary: 'Successful', detail: 'Checkout!', life: 3000});
+                    this.address = new Address();
+            
+                    this.saleTransactionLineItems = [];
+            
+                    this.sessionService.setCartLineItems(this.saleTransactionLineItems);
+                    this.totalLineItem = 0;
+                    this.totalPriceAmount = 0;
+                    window.location.reload();
+
+                    this.displayRegisteredGuestCheckoutDialog = false;
+
+                },
+                error: (error) => {
+                    //   this.message = "An error has occurred while registering new account: \n" + error;
+                    // this.message = "This email is already registered as a customer!";
+                    this.messageService.add({severity:'error', summary: 'Error', detail: 'Error Checkout!', life: 3000});
+                    console.log('********** Checkout.ts: ' + error);
+                }
+            })
+        }
+                
         for (var lineItem of this.saleTransactionLineItems) {
             console.log(lineItem.item.itemId);
         }
@@ -192,5 +252,74 @@ export class ShoppingCartComponent implements OnInit {
         console.log("isPreorder: " + this.isPreorder);
 
     }
+
+    unregisteredGuestCheckout() {
+        const datepipe: DatePipe = new DatePipe('en-US')
+
+        let transactionDate = new Date().toISOString();
+        let collectionDate = this.collectionDateTime.toISOString();
+        let username = this.newUsername;
+        let firstName = this.newFirstName
+        let lastName = this.newLastName
+
+        if(username!='' && firstName!='' && lastName!='') {
+
+            var newSaleTransaction: SaleTransaction;
+
+            let validaCheckOut = false;
+
+            if(this.isSelfPickup==true) {
+                console.log("=== unregister self pickup checkout === ")
+                validaCheckOut = true;
+                
+                newSaleTransaction = new SaleTransaction(1, this.totalLineItem, this.totalQuantity,
+                    this.totalPriceAmount, transactionDate, collectionDate, this.isSelfPickup,
+                    null, false, this.isPreorder, false);
+                
+            } else if(this.isSelfPickup==false && this.address.line!='' && this.address.postCode!=''){
+                console.log("=== unregister delivery checkout === ")
+                validaCheckOut = true;
+                this.deliveryAddress = this.address.line + "; Postal Code: "+this.address.postCode;
+                
+                newSaleTransaction = new SaleTransaction(1, this.totalLineItem, this.totalQuantity,
+                    this.totalPriceAmount, transactionDate, collectionDate, this.isSelfPickup,
+                    this.deliveryAddress, false, this.isPreorder, false);
+            }
+
+            if(validaCheckOut) {
+                this.saleTransactionService.createNewSessionService(username, firstName, lastName, newSaleTransaction, 
+                    this.saleTransactionLineItems, this.items).subscribe({
+                    next: (response) => {
+                        this.messageService.add({severity:'success', summary: 'Successful', detail: 'Checkout!', life: 3000});
+                        this.address = new Address();
+                
+                        this.saleTransactionLineItems = [];
+                        this.sessionService.setCartLineItems(this.saleTransactionLineItems);
+                        this.displayUnregisteredGuestCheckoutDialog = false;
+                        window.location.reload();
+                        
+                    },
+                    error: (error) => {
+                        this.messageService.add({severity:'error', summary: 'Error', detail: 'Error Checkout!', life: 3000});
+                        console.log('********** Checkout.ts: ' + error);
+                    }
+                })
+            }
+                    
+            for (var lineItem of this.saleTransactionLineItems) {
+                console.log(lineItem.item.itemId);
+            }
+
+            console.log("totalLineItem: " + this.totalLineItem);
+            console.log("totalQuantity: " + this.totalQuantity);
+            console.log("totalPriceAmount: " + this.totalPriceAmount);
+            console.log("collectionDateTime: " + this.collectionDateTime);
+            console.log("isSelfPickup: " + this.isSelfPickup);
+            console.log("deliveryAddress: " + this.deliveryAddress);
+            console.log("isPreorder: " + this.isPreorder);
+
+        }
+    }
+
 
 }
