42905551c4c824fc750004503693506c287ccfd2
JordKong <e0540507@u.nus.edu>
2022-04-17 15:17:47 +0800

    Added CreateNewGiftCard method in view-gift-card-type that adds a gift card to cart

--- src/app/item-views/view-gift-card-type/view-gift-card-type.component.ts
+++ src/app/item-views/view-gift-card-type/view-gift-card-type.component.ts
@@ -8,6 +8,7 @@ import { GiftCard } from '../../models/gift-card';
 
 import { SaleTransactionLineItem } from '../../models/sale-transaction-line-item';
 import { MessageService } from 'primeng/api';
+import {FileUploadModule} from 'primeng/fileupload';
 
 @Component({
   selector: 'app-view-gift-card-type',
@@ -19,6 +20,11 @@ export class ViewGiftCardTypeComponent implements OnInit {
   giftCardTypeId: string | null;
   giftCardTypeToView: GiftCardType | null;
   retrieveGiftCardTypeError: boolean;
+  displayCreateGiftCardDialog : boolean;
+  createGiftCardError : boolean;
+  newGiftCard: GiftCard;
+  message: string | undefined;
+  createGiftCardSuccess: boolean;
 
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
@@ -44,6 +50,47 @@ export class ViewGiftCardTypeComponent implements OnInit {
         }
       });
     }
+  }
+
+  createNewGiftCard() {
+    if (this.newGiftCard.message != null) {
+      this.newGiftCard.giftCardType = this.giftCardTypeToView;
+      this.addProductToCart(this.newGiftCard);
+    }
+    else {
+      this.createGiftCardError = true;
+      this.message = "Creation failed: No message written!";
+    }
+  }
+
+  // ADD TO CART
+  addProductToCart(giftCard: GiftCard) {
+    let cartLineItems: SaleTransactionLineItem[];
+    cartLineItems = this.sessionService.getCartLineItems();
+    var newLineItem: SaleTransactionLineItem;
+    newLineItem = new SaleTransactionLineItem();
+
+    // add to cart
+    console.log("cartLineItems.length?? " + cartLineItems.length)
+    // newLineItem = new SaleTransactionLineItem(cartLineItems.length + 1, 1, 100);
+    newLineItem = new SaleTransactionLineItem(cartLineItems.length + 1, 1, giftCard.giftCardType?.unitPrice);
+    newLineItem.item = giftCard;
+    cartLineItems.push(newLineItem);
+    this.sessionService.setCartLineItems(cartLineItems);
+    console.log("inside here, item not already inside cart")
+    console.log("line item price: " + newLineItem.unitPrice)
+    console.log("line item serial number??: " + newLineItem.serialNumber)
+    this.messageService.add({severity:'success', summary: 'Successful', detail: 'Added item to cart!', life: 3000});
+
+    this.message = "You have successfully created a gift card and added it to your cart!";
+    this.createGiftCardSuccess = true;
+  }
+
+  showCreateGiftCardDialog() {
+    this.displayCreateGiftCardDialog = true;
+    this.createGiftCardError = false;
+    this.newGiftCard = new GiftCard();
+    this.createGiftCardSuccess = false;
   }
 
 }
