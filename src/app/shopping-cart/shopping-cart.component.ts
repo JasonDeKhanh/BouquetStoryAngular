d9c70bdfd7af008c2f54979bbab12ecf7d0223d5
Qingyi <xqy1115@gmail.com>
2022-04-17 14:57:39 +0800

    cc card

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -95,7 +95,7 @@ export class ShoppingCartComponent implements OnInit {
                 this.creditCards = response;
 
                 for(var cc of this.creditCards) {
-                    let ccnum = cc.ccNum.length == 16 ? "******"+cc.ccNum.toString().substring(13, 17) : cc.ccNum;
+                    let ccnum = cc.ccNum.length == 16 ? "************"+cc.ccNum.toString().substring(13, 18) : cc.ccNum;
                     this.listCreditCards.push(
                         { label: ccnum, value: cc }
                     )
