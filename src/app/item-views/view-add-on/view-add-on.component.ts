1513ba846e6746d3443611b63c9b7ef33b5aa7da
JordKong <e0540507@u.nus.edu>
2022-04-17 12:00:24 +0800

    Added Layout for view-add-on

--- src/app/item-views/view-add-on/view-add-on.component.ts
+++ src/app/item-views/view-add-on/view-add-on.component.ts
@@ -5,6 +5,7 @@ import { SessionService } from '../../services/session.service';
 import { AddOnService } from '../../services/add-on.service';
 import { AddOn } from '../../models/add-on';
 
+import { SaleTransactionLineItem } from '../../models/sale-transaction-line-item';
 import { MessageService } from 'primeng/api';
 
 @Component({
@@ -20,7 +21,9 @@ export class ViewAddOnComponent implements OnInit {
 
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
-              public addOnService: AddOnService)
+              public addOnService: AddOnService,
+              private sessionService: SessionService,
+              private messageService: MessageService)
     {
       this.addOnId = null;
       this.addOnToView = new AddOn();
@@ -40,6 +43,51 @@ export class ViewAddOnComponent implements OnInit {
         }
       });
     }
+  }
+
+  // ADD TO CART
+  addProductToCart(addOn: AddOn) {
+    let cartLineItems: SaleTransactionLineItem[];
+    cartLineItems = this.sessionService.getCartLineItems();
+    var itemAlreadyAdded: boolean;
+    itemAlreadyAdded = false;
+    var newLineItem: SaleTransactionLineItem;
+    newLineItem = new SaleTransactionLineItem();
+
+    // check if item is already inside cart
+    for (var lineItem of cartLineItems) {
+        if (lineItem.item?.itemId === addOn.itemId) {
+            itemAlreadyAdded = true;
+            newLineItem = lineItem;
+            break;
+        }
+    }
+
+    if (itemAlreadyAdded === false) {
+        console.log("cartLineItems.length?? " + cartLineItems.length)
+        newLineItem = new SaleTransactionLineItem(cartLineItems.length + 1, 1, addOn.unitPrice);
+        newLineItem.item = addOn;
+        cartLineItems.push(newLineItem);
+        this.sessionService.setCartLineItems(cartLineItems);
+        console.log("inside here, item not already inside cart")
+        console.log("line item price: " + newLineItem.unitPrice)
+        console.log("line item serial number??: " + newLineItem.serialNumber)
+        
+        this.messageService.add({severity:'success', summary: 'Successful', detail: 'Added item to cart!', life: 3000});
+    }
+    else // item already in cart 
+    {
+        console.log("inside here, item IS ALREADY inside cart")
+        var curQuantity = newLineItem.quantity;
+
+        (newLineItem.quantity && curQuantity) ? newLineItem.quantity = curQuantity + 1 : null;
+
+        this.sessionService.setCartLineItems(cartLineItems);
+        
+        this.messageService.add({severity:'success', summary: 'Successful', detail: 'Added item to cart!', life: 3000});
+
+    }
+
   }
 
 }
